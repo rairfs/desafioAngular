a884f76db307c5d2098f92eb45a57ab944fb4d21
Rai Rafael <rairafaelss@gmail.com>
2023-03-03 22:44:34 -0300

    Ajustado nav

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent {
+
+  @Input() pagina: number;
+
+  constructor() {
+    this.pagina = -1;
+  }
 
 }
